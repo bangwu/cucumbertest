239a264b2ee3fb986fdd069569e39e5840de9ba1
wubang <wolfwubang@gmail.com>
2015-02-28 16:08:14 +0800

    refactor the account creation feature

---
+++ test2/features/step_definitions/account_creation.rb
@@ -0,0 +1,8 @@
+Given(/^I try to create an account with password "(.*?)"$/) do |arg1|
+end
+
+Then(/^I should see that the password is invalid$/) do
+end
+
+Then(/^I should see that the password is valid$/) do
+end
