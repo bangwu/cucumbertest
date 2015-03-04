a6ae1c2829ed75aec477c6df1037deb15087a66b
wubang <wolfwubang@gmail.com>
2015-03-04 16:30:32 +0800

    add the crash_overdraw feature

---
+++ crash_withdraw/features/step_definistions/crash_overdraw.rb
@@ -0,0 +1,11 @@
+Given(/^my account has been credited with \$(\d+)$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^nothing should be dispensed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be told that I have insufficient funds in my account$/) do
+  pending # express the regexp above with the code you wish you had
+end
